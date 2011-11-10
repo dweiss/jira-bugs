180c4ef395ed1deeed53b3e635010e8e441abefd
Dawid Weiss <dawid.weiss@carrotsearch.com>
2011-11-10 20:27:08 +0100

    initial.

---
+++ src/test/java/com/carrotsearch/bugsinjira/SuiteAggregatorTest.java
@@ -0,0 +1,23 @@
+package com.carrotsearch.bugsinjira;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+{
+    SuiteAggregatorTest.Test1.class,
+    SuiteAggregatorTest.Test2.class,
+})
+public class SuiteAggregatorTest
+{
+    public static class Test1 {
+        @Test
+        public void method1() {}
+    }
+    
+    public static class Test2 extends Test1 {
+    }    
+}
